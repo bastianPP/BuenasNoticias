b59911942c12ec3b784da484718d29e7046af99b
SEBASTIAN GONZALEZ <paramopdr@Pedros-Mac.local>
2020-01-21 01:38:59 -0800

    Create ViewTableCell and custom TitleLabel and DescriptionLabel

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by Pedro Paramo on 1/21/20.
+//  Copyright © 2020 Pedro Paramo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
