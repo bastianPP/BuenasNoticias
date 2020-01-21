1afba50f873604c434c1586bf59dbd0227b10201
SEBASTIAN GONZALEZ <paramopdr@Pedros-Mac.local>
2020-01-21 02:35:34 -0800

    Enlazar el modelo y la vista con el ViewModel

--- GoodNews/Controllers/NewsListTableViewController.swift
+++ GoodNews/Controllers/NewsListTableViewController.swift
@@ -10,6 +10,8 @@ import Foundation
 import UIKit
 
 class NewsListTableViewController: UITableViewController {
+    private var articleListVM: ArticleListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -20,10 +22,37 @@ class NewsListTableViewController: UITableViewController {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         
         let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apikey=5757ff098e6743a3973facffd0f9e116")!
+        
         let webservice = Webservice()
-        webservice.getArticles(url: url) { _ in
-            
+        webservice.getArticles(url: url) { articles in
+            if let articles = articles {
+                self.articleListVM = ArticleListViewModel(articles: articles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
         }
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell not found")
+        }
+        
+        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+        
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        
+        return cell
+    }
 }
